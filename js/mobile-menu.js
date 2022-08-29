db54d0bd0bb3bc6b6986c655426abfb1c8df42c9
Oleksandr  Vovk <vovk.sasha@outlook.com>
2022-08-29 18:11:03 +0700

    finished section activity

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -3,12 +3,18 @@
     openMenuBtn: document.querySelector(".header__menu-open"),
     closeMenuBtn: document.querySelector(".mob-menu__close"),
     menu: document.querySelector(".mob-menu"),
+    body: document.querySelector("body"),
+    menuList: document.querySelector(".mob-menu__nav-list"),
   };
 
   refs.openMenuBtn.addEventListener("click", toggleMenu);
   refs.closeMenuBtn.addEventListener("click", toggleMenu);
+  refs.menuList.addEventListener("click", removeMenu);
 
   function toggleMenu() {
     refs.menu.classList.toggle("is-hidden");
+  }
+  function removeMenu() {
+    refs.menu.classList.add("is-hidden");
   }
 })();
